7938baf9d7abbe24b7b4258eaa26272027e8d45b
renon.k <renonk@icloud.com>
2021-06-14 17:00:03 +0900

    コミット

---
+++ SecondKadaiApp/ResultViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ResultViewController.swift
+//  SecondKadaiApp
+//
+//  Created by Kakunaka Renon on 2021/06/13.
+//
+
+import UIKit
+
+class ResultViewController: UIViewController {
+    
+    @IBOutlet weak var label: UILabel!
+    
+    var name:String = ""
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let result = name
+        label.text = "こんにちは\(name)さん "
+
+        // Do any additional setup after loading the view.
+    }
+}
